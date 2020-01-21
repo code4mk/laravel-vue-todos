9c10ff76b8da516b517a0be145fbfc2643806f06
mostafa <radionishi@gmail.com>
2020-01-21 15:07:12 +0600

    todos deploy heroku

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <title>Todos</title>
         <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://laravel-vue-todos.herokuapp.com/css/app.css">
     </head>
     <body>
         <div class="" id="app">
             <todo-component></todo-component>
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="https://laravel-vue-todos.herokuapp.com/js/app.js"></script>
     </body>
 </html>
